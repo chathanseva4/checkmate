fb5a0bb9b29212d4f87f56733667d23fb899a635
chathanseva4 <115425908+chathanseva4@users.noreply.github.com>
2022-10-10 00:52:56 +0530

    Final file uploaded

---
+++ punchedcards.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int x=t;
+    while(t--){
+        int r,c;
+        cin>>r>>c;
+        r=r*2+1;
+        c=c*2+1;
+        cout<<"Case #"<<x-t<<endl;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                // cout<<"|"<<" "<<". ";
+                if(i==0 && j==0 || i==0 && j==1 || i==1 && j==0 ) cout<<".";
+                else if(i%2==0){
+                    if(j%2==0)cout<<"+";
+                    else cout<<"-";
+                }
+                else{
+                    if(j%2==0) cout<<"|"; 
+                    else cout<<".";
+                }
+            }
+            cout<<endl;
+        }
+    }
+}
